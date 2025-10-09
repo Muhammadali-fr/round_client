3da70e00695e112385509658a28aacb069d625c0
Muhammadali <boburjonr934@gmail.com>
2025-10-09 08:41:32 +0500

    product server page

--- src/api/services/products.ts
+++ src/api/services/products.ts
@@ -2,9 +2,16 @@
 // fetcher function 
 import { fetcher } from "../fetcher";
 
-// get user 
+// get products 
 export function getProducts() {
     return fetcher('/product', {
         method: 'GET',
     });
 }
+
+// get one product 
+export function getOneProduct(id: string) {
+    return fetcher(`/product/${id}`, {
+        method: 'GET'
+    });
+};
--- src/app/(pages)/product/[id]/page.tsx
+++ src/app/(pages)/product/[id]/page.tsx
@@ -1,7 +1,33 @@
-export default function Product(){
+
+// next 
+import Link from "next/link";
+
+// Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Thumbs } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/thumbs";
+
+
+// services 
+import { getOneProduct } from "@/src/api/services/products"
+
+export default async function Product({ params }: { params: { id: string } }) {
+
+    const { id } = await params;
+
+    const { product, related } = await getOneProduct(id);
+    console.log(product, related)
+
     return (
-        <div>
-            adkaskldasl
+        <div className="custom-width space-y-5 py-5">
+            {/* Breadcrumb */}
+            <nav className="text-sm text-gray-500 flex gap-1">
+                <Link href="/">Home</Link> / <Link href="/">products</Link> / <p className="line-clamp-1">{product.name}</p>
+            </nav>
+
         </div>
     )
 }
