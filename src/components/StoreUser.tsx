55e6110ed4d76179b72efbc8f7a8aa19ab2bcb41
Muhammadali <boburjonr934@gmail.com>
2025-11-06 09:38:30 +0500

    store user fix

--- src/components/StoreUser.tsx
+++ src/components/StoreUser.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 // react and next 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useState } from "react";
 
 // services 
 import { getUser, authRefreshToken } from "../api/services/auth";
@@ -13,58 +12,80 @@ import { useDispatch } from "react-redux";
 import { setUser } from "../lib/features/userSlice";
 import { setUserProducts } from "../lib/features/userProducts";
 
-// loader and toast 
-import toast from "react-hot-toast";
-
 // assets 
 import LogoImage from '@/public/assets/logo.svg';
-import { UserProp } from "../types/user";
+
+// tanstack query 
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export default function StoreUser() {
-  // states 
-  const [loading, setLoading] = useState(false);
-
   // redux 
   const dispatch = useDispatch();
 
-  // veriables 
-  const router = useRouter();
+  // states 
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [refreshToken, setRefreshToken] = useState<string | null>(null);
 
-  // functions 
-  const getUserFunction = async () => {
-    // tokens 
-    const accessToken = localStorage.getItem('accessToken');
-    const refreshToken = localStorage.getItem('refreshToken');
-    
-    try {
-      setLoading(true);
-      const { user, success }: {user: UserProp, success: boolean} = await getUser({ token: accessToken });
-      if (success) {
-        dispatch(setUser(user));
-        dispatch(setUserProducts(user.products));
-      } else {
-        const { accessToken, success } = await authRefreshToken({ token: refreshToken });
-        if (success) {
-          toast('wait we are logging again into your account...');
-          localStorage.setItem('accessToken', accessToken);
-          return window.location.reload();
-        };
-        // router.push('auth/login');
-      };
-    } catch (error) {
-      console.log(error);
-      toast('something went wrong login again')
-      router.replace('/auth/login');
-    } finally { setLoading(false) };
-  };
-
-  // useEffects 
+  // tokens 
   useEffect(() => {
-    getUserFunction();
+    if (typeof window !== 'undefined') {
+      setAccessToken(localStorage.getItem("accessToken"));
+      setRefreshToken(localStorage.getItem("refreshToken"));
+    };
   }, []);
 
+  const { data, isPending } = useQuery({
+    queryKey: ['user'],
+    queryFn: () => getUser({ token: accessToken }),
+    enabled: !!accessToken,
+    retry: false
+  });
+
+  if (isPending) {
+    return (
+      <div className="fixed inset-0 flex flex-col items-center justify-between bg-white z-50 py-5">
+        <span />
+        <Image src={LogoImage} alt="logo" width={120} height={120} className="rounded-full" />
+        <p className="text-black">Loading your account...</p>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    if (data?.success) {
+      dispatch(setUser(data.user));
+      dispatch(setUserProducts(data.user.products));
+      return;
+    };
+  }, [data]);
+
+  const { data: refreshData, isPending: refreshPending } = useQuery({
+    queryKey: ['reUser'],
+    queryFn: () => authRefreshToken(refreshToken),
+  });
+  console.log(refreshData);
+
+
+
+  // // console.log(data);
+  // if (data?.success) {
+  //   dispatch(setUser(data?.user));
+  //   dispatch(setUserProducts(data?.user?.products));
+  // } else {
+
+  //   const { data: refreshData, isPending: refreshPending, error: refreshError } = useQuery({
+  //     queryKey: ['reUser'],
+  //     queryFn: () => authRefreshToken(refreshToken),
+  //   });
+
+  //   if (refreshData?.success) {
+  //     localStorage.setItem('accessToken', refreshData?.accessToken);
+  //   };
+  // };
+
   // returns 
-  if (loading) {
+  if (isPending) {
     return (
       <div className="fixed inset-0 flex flex-col items-center justify-between bg-white z-50 py-5">
         <span />
