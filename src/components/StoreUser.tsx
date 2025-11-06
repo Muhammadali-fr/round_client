1386b5d6befc9e2c3e0603b668d80b3a31b702fc
Muhammadali <boburjonr934@gmail.com>
2025-11-06 11:29:27 +0500

    some error

--- src/components/StoreUser.tsx
+++ src/components/StoreUser.tsx
@@ -2,10 +2,10 @@
 
 // react and next 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // services 
-import { getUser, authRefreshToken } from "../api/services/auth";
+import { getUser } from "../api/services/auth";
 
 // redux 
 import { useDispatch } from "react-redux";
@@ -17,32 +17,33 @@ import LogoImage from '@/public/assets/logo.svg';
 
 // tanstack query 
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
 
 export default function StoreUser() {
   // redux 
   const dispatch = useDispatch();
 
-  // states 
-  const [accessToken, setAccessToken] = useState<string | null>(null);
-  const [refreshToken, setRefreshToken] = useState<string | null>(null);
 
   // tokens 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setAccessToken(localStorage.getItem("accessToken"));
-      setRefreshToken(localStorage.getItem("refreshToken"));
-    };
-  }, []);
+  const accessToken = typeof window === 'undefined' ? localStorage.getItem('accessToken') : null;
+  const refreshToken = typeof window === 'undefined' ? localStorage.getItem('refreshToken') : null;
 
-  const { data, isPending } = useQuery({
-    queryKey: ['user'],
+  const accessQuery = useQuery({
+    queryKey: ['user', accessToken],
     queryFn: () => getUser({ token: accessToken }),
     enabled: !!accessToken,
     retry: false
   });
 
-  if (isPending) {
+
+  useEffect(() => {
+    if (accessQuery.data?.success) {
+      dispatch(setUser(accessQuery.data.user));
+      dispatch(setUserProducts(accessQuery.data.user.products));
+    };
+  }, [accessQuery.data, dispatch]);
+
+
+  if (accessQuery.isPending) {
     return (
       <div className="fixed inset-0 flex flex-col items-center justify-between bg-white z-50 py-5">
         <span />
@@ -52,48 +53,5 @@ export default function StoreUser() {
     );
   };
 
-  useEffect(() => {
-    if (data?.success) {
-      dispatch(setUser(data.user));
-      dispatch(setUserProducts(data.user.products));
-      return;
-    };
-  }, [data]);
-
-  const { data: refreshData, isPending: refreshPending } = useQuery({
-    queryKey: ['reUser'],
-    queryFn: () => authRefreshToken(refreshToken),
-  });
-  console.log(refreshData);
-
-
-
-  // // console.log(data);
-  // if (data?.success) {
-  //   dispatch(setUser(data?.user));
-  //   dispatch(setUserProducts(data?.user?.products));
-  // } else {
-
-  //   const { data: refreshData, isPending: refreshPending, error: refreshError } = useQuery({
-  //     queryKey: ['reUser'],
-  //     queryFn: () => authRefreshToken(refreshToken),
-  //   });
-
-  //   if (refreshData?.success) {
-  //     localStorage.setItem('accessToken', refreshData?.accessToken);
-  //   };
-  // };
-
-  // returns 
-  if (isPending) {
-    return (
-      <div className="fixed inset-0 flex flex-col items-center justify-between bg-white z-50 py-5">
-        <span />
-        <Image src={LogoImage} alt="logo" width={120} height={120} className="rounded-full" />
-        <p className="text-black">Loading your account...</p>
-      </div>
-    );
-  }
-
   return null;
-}
+};
