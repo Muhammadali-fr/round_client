84df7218c254915359ee0818ac5f461493756371
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2025-11-08 21:10:08 +0500

    fix

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -9,6 +9,9 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/src/lib/store";
 import { Form, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import { useState } from "react";
+
 
 export default function UserSettings() {
 
@@ -25,17 +28,39 @@ export default function UserSettings() {
     return (
         <div className="custom-width border rounded-xl p-5 my-5">
             <h1 className="text-xl font-bold text-gray-600 mb-2">Edit User</h1>
-            <Form {...form}>
-                <FormField control={form.control} name="image" render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>
-                            rasm tabnla
-                            <Input type="file" />
-                        </FormLabel>
-                    </FormItem>
-                )}>
-                </FormField>
-            </Form>
+            <div className="flex items-start">
+
+                {/* select image */}
+                <Form {...form}>
+                    <FormField control={form.control} name="image" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                <label className="cursor-pointer" htmlFor="ImageInput">
+                                    <Input className="hidden" type="file" id="ImageInput" />
+                                    <Image className="w-25 h-25 rounded-full" src={user?.profile} alt={user?.name} width={70} height={70} />
+                                </label>
+                            </FormLabel>
+                        </FormItem>
+                    )}>
+                    </FormField>
+                </Form>
+
+
+                {/* name */}
+                <Form {...form}>
+                    <FormField control={form.control} name="name" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="flex flex-col items-start">
+                                <p>Your name</p>
+                                <Input placeholder={user?.name} />
+                            </FormLabel>
+                        </FormItem>
+                    )}>
+
+                    </FormField>
+                </Form>
+
+            </div>
         </div>
     );
 };
