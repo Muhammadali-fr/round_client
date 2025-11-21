8efde306925353cb03dbd5ad38338be16d52b74c
Muhammadali <boburjonr934@gmail.com>
2025-11-21 09:04:17 +0500

    fix inpt

--- src/app/(root)/shop/components/Search.tsx
+++ src/app/(root)/shop/components/Search.tsx
@@ -1,41 +1,46 @@
+"use client";
 
-'use client'
-
-// react or next 
+// react or next
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useDebouncedCallback } from 'use-debounce'
+import { useDebouncedCallback } from "use-debounce";
 
-// component 
+// component
 import { Input } from "@/components/ui/input";
 
-// lucide react 
+// lucide react
 import { Search, X } from "lucide-react";
 
 export default function ShopPageSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { replace } = useRouter();
+  const handleChange = useDebouncedCallback((term: string) => {
+    const param = new URLSearchParams(searchParams);
+    if (term) {
+      param.set("query", term);
+    } else {
+      param.delete("query");
+    }
+    replace(`${pathname}?${param.toString()}`);
+  }, 300);
 
-    const handleChange = useDebouncedCallback((term: string) => {
-        const param = new URLSearchParams(searchParams);
-        if (term) {
-            param.set('query', term);
-        } else {
-            param.delete('query');
-        };
-        replace(`${pathname}?${param.toString()}`);
-    }, 300);
-
-    return (
-        <div>
-            <label className="relative">
-                <Search className="absolute -top-[2px] left-3 text-gray-400" />
-                <Input onChange={e => handleChange(e.target.value)} defaultValue={searchParams.get('query')?.toString()} type="text" autoFocus className="border border-violet-400 w-full p-2 pl-10 rounded-lg outline-none outline-violet-700" placeholder="Search for products..." />
-                <div className="bg-gray-500 p-1 rounded-full absolute top-0 right-2 cursor-pointer hover:bg-gray-400">
-                    <X className="font-bold" size={12} color="#FFFFFF" />
-                </div>
-            </label>
+  return (
+    <div>
+      <label className="relative">
+        <Search className="absolute -top-[2px] left-3 text-gray-400" />
+        <Input
+          onChange={(e) => handleChange(e.target.value)}
+          defaultValue={searchParams.get("query")?.toString()}
+          type="text"
+          autoFocus
+          className="border border-violet-400 w-full p-2 pl-10 rounded-lg outline-none outline-violet-700"
+          placeholder="Search for products..."
+        />
+        <div className="bg-gray-500 p-1 rounded-full absolute top-0 right-2 cursor-pointer hover:bg-gray-400">
+          <X className="font-bold" size={12} color="#FFFFFF" />
         </div>
-    )
-};
+      </label>
+    </div>
+  );
+}
