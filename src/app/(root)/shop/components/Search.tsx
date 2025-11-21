76e8ddc4be5d56276cd515ba89fcd861aa760ce3
Muhammadali <boburjonr934@gmail.com>
2025-11-21 09:45:24 +0500

    clear button function finished

--- src/app/(root)/shop/components/Search.tsx
+++ src/app/(root)/shop/components/Search.tsx
@@ -9,11 +9,13 @@ import { Input } from "@/components/ui/input";
 
 // lucide react
 import { Search, X } from "lucide-react";
+import { useState } from "react";
 
 export default function ShopPageSearch() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const [value, setvalue] = useState(searchParams.get('query') || "");
 
   const handleChange = useDebouncedCallback((term: string) => {
     const param = new URLSearchParams(searchParams);
@@ -25,21 +27,33 @@ export default function ShopPageSearch() {
     replace(`${pathname}?${param.toString()}`);
   }, 300);
 
+  const clearQuery = () => {
+    setvalue("");
+    const param = new URLSearchParams(searchParams);
+    param.delete("query");
+    replace(`${pathname}?${param.toString()}`);
+  };
+
   return (
     <div>
       <label className="relative">
         <Search className="absolute -top-[2px] left-3 text-gray-400" />
         <Input
-          onChange={(e) => handleChange(e.target.value)}
-          defaultValue={searchParams.get("query")?.toString()}
+          onChange={(e) => { handleChange(e.target.value); setvalue(e.target.value) }}
+          value={value}
           type="text"
           autoFocus
           className="border border-violet-400 w-full p-2 pl-10 rounded-lg outline-none outline-violet-700"
           placeholder="Search for products..."
         />
-        <div className="bg-gray-500 p-1 rounded-full absolute top-0 right-2 cursor-pointer hover:bg-gray-400">
-          <X className="font-bold" size={12} color="#FFFFFF" />
-        </div>
+
+        {
+          value &&
+          <div onClick={clearQuery} className="bg-gray-500 p-1 rounded-full absolute top-0 right-2 cursor-pointer hover:bg-gray-400">
+            <X className="font-bold" size={12} color="#FFFFFF" />
+          </div>
+        }
+
       </label>
     </div>
   );
