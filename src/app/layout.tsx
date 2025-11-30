e90baa928cdbea1ea28f806dd378539d568db160
Muhammadali <boburjonr934@gmail.com>
2025-11-30 11:37:42 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f6f5f8]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* tansctack provider  */}
         <TanstackQueryProvider>
